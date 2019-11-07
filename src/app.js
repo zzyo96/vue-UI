c79b0f3e07a559a9a16cf617ea08b5af368f72f0
zzyo <1977795940@qq.com>
2019-11-07 16:15:27 +0800

    添加input的测试用例

--- src/app.js
+++ src/app.js
@@ -15,6 +15,18 @@ new Vue({
     loading1: false,
     loading2: true,
     loading3: false,
+  },
+  created(){
+    setTimeout(()=>{
+      let event = new Event('change');
+      let inputElement = this.$el.querySelector('input')
+      inputElement.dispatchEvent(event)
+    },3000)
+  },
+  methods: {
+    inputChange(e) {
+      console.log(e)
+    }
   }
 });
 
