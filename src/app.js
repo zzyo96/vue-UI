aea2d2fcb58824925c4397ef7d0523c19f31bae8
zzyo <1977795940@qq.com>
2019-11-08 14:54:58 +0800

    完成row和col的雏形

--- src/app.js
+++ src/app.js
@@ -3,11 +3,15 @@ import Button from './button'
 import Icon from './icon'
 import ButtonGroup from './button-group'
 import Input from './input'
+import Row from './row'
+import Col from './col'
 
 Vue.component('g-button',Button);
 Vue.component('g-icon',Icon);
 Vue.component('g-button-group', ButtonGroup);
-Vue.component('g-input', Input)
+Vue.component('g-input', Input);
+Vue.component('g-row', Row);
+Vue.component('g-col', Col);
 
 new Vue({
   el:'#app',
@@ -18,11 +22,6 @@ new Vue({
     message: 'hi'
   },
   created(){
-    setTimeout(()=>{
-      let event = new Event('change');
-      let inputElement = this.$el.querySelector('input')
-      inputElement.dispatchEvent(event)
-    },3000)
   },
   methods: {
     inputChange(e) {
